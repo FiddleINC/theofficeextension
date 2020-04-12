fa036dd7b665d354b0fc6a0657c50d12158cea46
Prachurjya Biswas <biswasprachurjya@gmail.com>
2020-04-12 18:46:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import quotes from 'the-office';
 import './App.css';
 
+function getQuotes() {
+	var quotedata = {
+		quote: '',
+		season: '',
+		episode: '',
+		character: ''
+	};
+	const index = Math.floor(Math.random() * quotes.length);
+	quotedata.season = quotes[index].season;
+	quotedata.episode = quotes[index].title;
+	const sceneIndex = Math.floor(Math.random() * quotes[index].scenes.length);
+	const scene = quotes[index].scenes[sceneIndex];
+	const lineIndex = Math.floor(Math.random() * scene.length);
+	quotedata.character = scene[lineIndex].character;
+	quotedata.quote = scene[lineIndex].line;
+	return quotedata;
+}
+
 function App() {
+	const [ quote, setQuote ] = useState('');
+	const [ season, setSeason ] = useState('');
+	const [ episode, setEpisode ] = useState('');
+	const [ character, setCharacter ] = useState('');
+
+	useEffect(() => {
+		const quoteData = getQuotes();
+		setQuote(quoteData.quote);
+		setSeason(quoteData.season);
+		setEpisode(quoteData.episode);
+		setCharacter(quoteData.character);
+		console.log('UseEffect Working');
+		return () => {};
+	}, []);
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<div className="App-header">
+				<div className="w3-container">
+					<h3 className="w3-panel w3-leftbar w3-xxlarge">{quote}</h3>
+					<h4>- {character}</h4>
+					<p>
+						<i>
+							Season {season} : {episode}
+						</i>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
